8511612de095177da59bfbc02ce5fb3d776af221
Ben West <bewest@gmail.com>
2016-01-25 17:38:28 -0800

    tweak init script

--- bin/openaps-oref0-setup-loop.sh
+++ bin/openaps-oref0-setup-loop.sh
@@ -4,6 +4,10 @@
 PUMP=pump
 CGM=cgm
 BASE=$(dirname $OPENAPS_HOME)
+MEDTRONIC_TRANSPORT=${MEDTRONIC_TRANSPORT-'subg_rfspy'}
+
+MEDTRONIC_SERIAL=$1
+DEXCOM_SERIAL=$2
 
 function setup_mmeowlink ( ) {
 SERIAL=$1
@@ -36,8 +40,16 @@ EOT
 (
 cd $BASE
 
-setup_mmeowlink $1
-setup_dexcom_ble_device $2
+case "$MEDTRONIC_TRANSPORT" in
+  subg_rfspy)
+    setup_mmeowlink $MEDTRONIC_SERIAL
+    ;;
+  *)
+    logger -t $0 "unsupported MEDTRONIC_TRANSPORT=$MEDTRONIC_TRANSPORT"
+    ;;
+esac
+
+setup_dexcom_ble_device $DEXCOM_SERIAL
 
 monitor_reports
 
